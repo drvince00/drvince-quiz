497b9aed3362f6ff03cc8bb385dc3b4003ec8dce
drvince <drvince00@gmail.com>
2024-11-12 22:31:13 +0900

    addsense tag added.

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Outfit } from 'next/font/google';
 import './globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Script from 'next/script';
 
 const outfit = Outfit({
   subsets: ['latin'],
@@ -20,6 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-5S66HQDXH4"></script>
         <script
@@ -32,8 +35,14 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-        {/* End Google Analytics */}
-        <link rel="icon" href="/favicon.ico" />
+
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4889325696719646"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className={outfit.className}>
         {children}
